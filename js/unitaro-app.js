81718fa650e8efa70b546922162406f838ddc676
y-sakai <y-sakai@zyyx.jp>
2019-06-18 15:01:25 +0900

    add unitaro-app.js for application stub

---
+++ js/unitaro-app.js
@@ -0,0 +1,24 @@
+/**
+JavaScript Game framework Application stub
+
+oosamanet@gmail.com
+
+
+*/
+var APP = {
+  WIDTH: 320,
+  HEIGHT: 500,
+  init: function(){
+  },
+  update: function(age){
+    unitaro.dib.box(0,0,this.WIDTH,this.HEIGHT,"rgb(40,40,40)");
+    unitaro.dib.box(10,10,this.WIDTH-20,this.HEIGHT-20,"rgb(0,0,0)");
+    unitaro.dib.textout(50,20,"unitaroJS sample","yellow",100);
+  },
+  onclick: function(x,y){
+  }
+};
+
+window.addEventListener('load',function(){
+	window.app = new unitaro.App(APP);
+},false);
